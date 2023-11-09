5dc5679653616b60230c3badcdc1a4d235631d53
ChrisTarla <christine.tarla26@ycdsbk12.ca>
2023-11-09 09:30:40 -0500

    worked on task

--- Sketch.java
+++ Sketch.java
@@ -1,11 +1,12 @@
 /* Instructions:
- * Create a method with parameters to draw a composite object (consisting of various shapes) at various locations specified by the parameters 
- * Create another method with parameters to draw a composite object at various locations specified by the method parameters. 
+ * > Create a method with parameters to draw a composite object (consisting of various shapes) at various locations specified by the parameters 
+ * > Create another method with parameters to draw a composite object at various locations specified by the method parameters. 
  * Include additional parameters to modify the object(s) drawn in this method. i.e colour, size, etc.
  * Demonstration of method with parameters and a return value. i.e methods such that given dimensions of an object, returns the x value 
  * or y value for the object, to be drawn in the middle of the screen. 
  */
 
+import java.util.Random;
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
@@ -26,28 +27,61 @@ public class Sketch extends PApplet {
 
   // Called repeatedly, anything drawn to the screen goes here
   public void draw() {
-    flower(50, 50);
-    bee(100, 100, 2);
+    flower(375, 375, 25, 25);
+    bee(100, 100);
+    //while (i < 100){
+      //noLoop();
+    //}
   }
 
-  public void bee(int intBeeX, int intBeeY, int intStrokeWeight){
+  public void bee(int intBeeX, int intBeeY){
+    fill(185, 210, 215);
+    ellipse(intBeeX, intBeeY - 20, 15, 20);
     fill(255, 230, 0);
-    ellipse(intBeeX, intBeeY, 25, 25);
-    //strokeWeight(intStrokeWeight);
-    //line();
+    ellipse(intBeeX, intBeeY, 40, 25);
+    fill(0);
+    rect(intBeeX -13 , intBeeY - 11, 5, 22);
+    rect(intBeeX - 2, intBeeY - 12, 5, 25);
+    rect(intBeeX + 10, intBeeY - 10, 5, 20);
+    line(intBeeX + 15, intBeeY - 10, intBeeX + 25, intBeeY - 20);
+    
 
   }
 
-  public void flower(int intFlowerX, int intFlowerY){
+  public int flower(int intUpperX, int intUpperY, int intLowerX, int intLowerY){
+    int intColourSequence = 0;
+    int i = 0;
+    Random ranFlowerX = new Random();
+    Random ranFlowerY = new Random();
+    int intFlowerX = ranFlowerX.nextInt(((intUpperX - intLowerX) + 1) + intLowerX);
+    int intFlowerY = ranFlowerY.nextInt(((intUpperY - intLowerY) + 1) + intLowerY);
+    
     fill(50, 170, 80);
     rect(intFlowerX - 6, intFlowerY, 12, 50);
-    fill(60, 160, 200);
-    ellipse(intFlowerX - 12, intFlowerY - 12, 25, 25);
-    ellipse(intFlowerX - 12, intFlowerY + 12, 25, 25);
-    ellipse(intFlowerX + 12, intFlowerY - 12, 25, 25);
-    ellipse(intFlowerX + 12, intFlowerY + 12, 25, 25);
-    fill(252, 211, 0);
-    ellipse(intFlowerX, intFlowerY, 25, 25);
-  }
 
+    for (i = 0; i < 1; i ++){
+      if (intColourSequence == 0){
+        fill(60, 160, 200);
+        intColourSequence ++;
+      } else if (intColourSequence == 1){
+        fill(250, 15, 0);
+        intColourSequence ++;
+      } else if (intColourSequence == 2){
+        fill(190, 90, 240);
+        intColourSequence ++;
+      } else if (intColourSequence == 3){
+        fill(255, 200, 0);
+        intColourSequence -= 3;
+      }
+
+      ellipse(intFlowerX - 12, intFlowerY - 12, 25, 25);
+      ellipse(intFlowerX - 12, intFlowerY + 12, 25, 25);
+      ellipse(intFlowerX + 12, intFlowerY - 12, 25, 25);
+      ellipse(intFlowerX + 12, intFlowerY + 12, 25, 25);
+      fill(252, 211, 0);
+      ellipse(intFlowerX, intFlowerY, 25, 25);
+      
+    }
+    return i;
+  }
 }
